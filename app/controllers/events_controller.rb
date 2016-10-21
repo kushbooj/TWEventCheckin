5c86fa8e3431a680bbaa42dd76b4a952d10c5054
Kushboo Jain <kushbooj@thoughtworks.com>
2016-10-22 02:42:57 +0530

    Sending the generated QRCode in mail

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 require 'rqrcode'
+require 'mail'
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy, :generate]
 
@@ -66,8 +67,26 @@ class EventsController < ApplicationController
     participants = Participant.all.select { |m| m.event_id == @event.id }
     pp participants
     participants.each do |participant|
-      qrcode = RQRCode::QRCode.new(participant.email + @event.id.to_s, :size => 4, :level => :h)
-      QrCode.create({:event => @event, :participant => participant, :code => qrcode})
+      qrcode = RQRCode::QRCode.new(participant.email + @event.id.to_s, :size => 4, :level => :h).as_png(
+          resize_gte_to: false,
+          resize_exactly_to: false,
+          fill: 'white',
+          color: 'black',
+          size: 120,
+          border_modules: 4,
+          module_px_size: 6,
+          file: nil # path to write
+      )
+      mail = Mail.new do
+        from    'kushbooj@thoughtworks.com'
+        to      participant.email
+        subject 'Scan the code for hassle free entry in'
+        body    qrcode.to_s
+      end
+
+      mail.delivery_method :sendmail
+
+      mail.deliver
     end
   end
 
