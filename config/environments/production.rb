81b6dfe8e80f4fa61f61735e9c51899fad8d1e61
Kushboo Jain <kushbooj@thoughtworks.com>
2016-11-24 10:44:47 +0530

    Changing the name of config vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => ENV['username'],
-      :password             => ENV['password'],
+      :user_name            => ENV['USERNAME'],
+      :password             => ENV['PASSWORD'],
       :authentication       => "plain",
       :enable_starttls_auto => true
   }
